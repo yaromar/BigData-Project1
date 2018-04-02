4e9a2e6155a423e585b6c94286c7023e3916ea9b
Victoria Zhong <SemicolonExpected@users.noreply.github.com>
2018-04-02 19:45:59 -0400

    Fixed problem. oops

--- RelationalDB/big-data-relational.py
+++ RelationalDB/big-data-relational.py
@@ -11,11 +11,11 @@ ADdb = MySQLdb.connect(host = 'localhost', user = 'bigdata', passwd = 'bigdata')
 cursor = ADdb.cursor()
 
 cursor.execute("CREATE DATABASE IF NOT EXISTS ADKnowledgeBase;")
-cursor.execute("CREATE TABLE IF NOT EXISTS ADKnowledgeBase.Patients(patient_id varchar(255), age int, gender char(1), education varchar(255));")
-cursor.execute("ALTER TABLE ADKnowledgeBase.Patients ADD PRIMARY KEY(patient_id);")
-cursor.execute("CREATE TABLE IF NOT EXISTS ADKnowledgeBase.PPI(a int, b int);")
-cursor.execute("CREATE TABLE IF NOT EXISTS ADKnowledgeBase.EntrezGeneSymbols( entrez_id int, gene_symbol varchar(255), gene_name varchar(255));")
-cursor.execute("ALTER TABLE ADKnowledgeBase.EntrezGeneSymbols ADD PRIMARY KEY(entrez_id);")
+cursor.execute("CREATE TABLE IF NOT EXISTS ADKnowledgeBase.Patients(patient_id varchar(255), age varchar(255), gender char(1), education varchar(255));")
+#cursor.execute("ALTER TABLE ADKnowledgeBase.Patients ADD PRIMARY KEY(patient_id);")
+cursor.execute("CREATE TABLE IF NOT EXISTS ADKnowledgeBase.PPI(a varchar(255), b varchar(255));")
+cursor.execute("CREATE TABLE IF NOT EXISTS ADKnowledgeBase.EntrezGeneSymbols( entrez_id varchar(255), gene_symbol varchar(255), gene_name varchar(255));")
+#cursor.execute("ALTER TABLE ADKnowledgeBase.EntrezGeneSymbols ADD PRIMARY KEY(entrez_id);")
 
 #create patient table
 #%s for strings %d for ints 
@@ -28,7 +28,7 @@ for row in csv_data:
 		firstline = False
 		continue
 	else:
-		cursor.execute('INSERT INTO ADKnowledgeBase.Patients(patient_id,age,gender,education) VALUES("row[0]","row[1]","row[2]","row[3]");')
+		cursor.execute('INSERT INTO ADKnowledgeBase.Patients(patient_id,age,gender,education) VALUES("%s","%s","%s","%s");', (row[0],row[1],row[2],row[3]))
 
 #create entrezGeneSymbols table
 csv_data = csv.reader(file('entrez_ids_genesymbol.csv'))
@@ -39,13 +39,13 @@ for row in csv_data:
 		firstline = False
 		continue
 	else:
-		cursor.execute('INSERT INTO ADKnowledgeBase.EntrezGeneSymbols(entrez_id, gene_symbol, gene_name) VALUES("row[0]","row[1]","row[2]");')
+		cursor.execute('INSERT INTO ADKnowledgeBase.EntrezGeneSymbols(entrez_id, gene_symbol, gene_name) VALUES("%s","%s","%s");',(row[0],row[1],row[2]))
 
 #create PPI table
 csv_data = csv.reader(file('PPI.csv'))
 
 for row in csv_data:
-	cursor.execute('INSERT INTO ADKnowledgeBase.PPI(a,b) VALUES("row[0]","row[1]");')
+	cursor.execute('INSERT INTO ADKnowledgeBase.PPI(a,b) VALUES("%s","%s");',(row[0],row[1]))
 
 ADdb.commit()
 #closes connection
