e4c3bfb56497980f748ed05fbc972db4c88c03e7
Victoria Zhong <SemicolonExpected@users.noreply.github.com>
2018-04-02 18:51:21 -0400

    Update big-data-relational.py
    Small issue when I try to load the csv it doesn't load the actual values and I get the error `Incorrect integer value <>`
    
    Will fix later. will work on query file now
    
    I already have the queries just need to make the interface.
    
    Queries would be
    
        SELECT B FROM ADKnowledgeBase.EntrezGeneSymbols INNER JOIN PPI
        ON entrez_id = a
        where a = query
    
    and 
    
        SELECT age, gender, education
        FROM ADKnowledgeBase.Patients
        WHERE patient_id = query

--- RelationalDB/big-data-relational.py
+++ RelationalDB/big-data-relational.py
@@ -1,2 +1,53 @@
 #!/usr/bin/python
 import MySQLdb
+import csv
+import cgitb
+
+#enable error logging 
+cgitb.enable(display = 0, logdir = "../logdir")
+
+ADdb = MySQLdb.connect(host = 'localhost', user = 'bigdata', passwd = 'bigdata')
+
+cursor = ADdb.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS ADKnowledgeBase;")
+cursor.execute("CREATE TABLE IF NOT EXISTS ADKnowledgeBase.Patients(patient_id varchar(255), age int, gender char(1), education varchar(255));")
+cursor.execute("ALTER TABLE ADKnowledgeBase.Patients ADD PRIMARY KEY(patient_id);")
+cursor.execute("CREATE TABLE IF NOT EXISTS ADKnowledgeBase.PPI(a int, b int);")
+cursor.execute("CREATE TABLE IF NOT EXISTS ADKnowledgeBase.EntrezGeneSymbols( entrez_id int, gene_symbol varchar(255), gene_name varchar(255));")
+cursor.execute("ALTER TABLE ADKnowledgeBase.EntrezGeneSymbols ADD PRIMARY KEY(entrez_id);")
+
+#create patient table
+#%s for strings %d for ints 
+csv_data = csv.reader(file('patients.csv'))
+
+#skip first row
+firstline = True;
+for row in csv_data:
+	if firstline:
+		firstline = False
+		continue
+	else:
+		cursor.execute('INSERT INTO ADKnowledgeBase.Patients(patient_id,age,gender,education) VALUES("row[0]","row[1]","row[2]","row[3]");')
+
+#create entrezGeneSymbols table
+csv_data = csv.reader(file('entrez_ids_genesymbol.csv'))
+
+firstline = True;
+for row in csv_data:
+	if firstline:
+		firstline = False
+		continue
+	else:
+		cursor.execute('INSERT INTO ADKnowledgeBase.EntrezGeneSymbols(entrez_id, gene_symbol, gene_name) VALUES("row[0]","row[1]","row[2]");')
+
+#create PPI table
+csv_data = csv.reader(file('PPI.csv'))
+
+for row in csv_data:
+	cursor.execute('INSERT INTO ADKnowledgeBase.PPI(a,b) VALUES("row[0]","row[1]");')
+
+ADdb.commit()
+#closes connection
+cursor.close()
+ 
